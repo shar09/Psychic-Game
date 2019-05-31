f469fb8316baa64271a984d39ee17fb4c7cffd0a
shar09 <sharathtelu9@gmail.com>
2019-05-30 22:43:31 -0500

    Game Created with no styling

---
+++ assets/game.js
@@ -0,0 +1,53 @@
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var userGuess=" ";
+var computerGuess;
+var userChoiceText=" ";
+var guessesLeftText;
+var guessesLeft=10;
+var winsStore;
+var lossesStore;
+var wins=0;
+var losses=0;
+computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
+
+function reset() {
+    this.computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
+    guessesLeft=10;   
+    userChoiceText=" "
+    userGuess=" ";
+};
+
+document.onkeyup = function(event) {   
+userGuess = event.key;
+
+if(userGuess!==computerGuess)
+{
+    userChoiceText = document.getElementById("userchoice-text");
+    userChoiceText.textContent = "Guessed so far: " +userGuess+ " , ";   
+    
+    guessesLeft--;
+    if(guessesLeft===0) 
+    {
+    losses++;
+    lossesStore = document.getElementById("losses");
+    lossesStore.textContent = "Losses: " +losses; 
+    reset();
+    }
+}
+
+else if(userGuess===computerGuess)
+{
+    wins++;
+    winsStore = document.getElementById("wins");
+    winsStore.textContent = "Wins: " +wins; 
+    reset();
+}
+
+guessesLeftText = document.getElementById("guesses-left")
+guessesLeftText.textContent = "Guesses Left: " +guessesLeft;
+};
+
+
+
