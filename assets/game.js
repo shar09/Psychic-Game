a2be8197d1d0d01e68d3675db419533ea610ac65
shar09 <sharathtelu9@gmail.com>
2019-05-30 23:28:19 -0500

    Game improved with slight styling

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,8 @@
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var userGuess=" ";
+var userGuess;
+var guessed=[];
 var computerGuess;
-var userChoiceText=" ";
+var userChoiceText;
 var guessesLeftText;
 var guessesLeft=10;
 var winsStore;
@@ -14,9 +15,8 @@ console.log(computerGuess);
 function reset() {
     this.computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(computerGuess);
+    guessed = [];
     guessesLeft=10;   
-    userChoiceText=" "
-    userGuess=" ";
 };
 
 document.onkeyup = function(event) {   
@@ -24,8 +24,9 @@ userGuess = event.key;
 
 if(userGuess!==computerGuess)
 {
+    guessed.push(userGuess);
     userChoiceText = document.getElementById("userchoice-text");
-    userChoiceText.textContent = "Guessed so far: " +userGuess+ " , ";   
+    userChoiceText.textContent = "Guessed so far: " +guessed;   
     
     guessesLeft--;
     if(guessesLeft===0) 
